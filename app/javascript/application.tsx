f53f7de387da1e8e348112efbab30465c5a94295
kmkkiii <rskkmkmk@gmail.com>
2022-10-06 19:10:28 +0900

    eslint, prettier設定

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -1,27 +1,23 @@
 // Entry point for the build script in your package.json
-import * as React from 'react'
-import { createRoot } from 'react-dom/client'
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider
-} from "@apollo/client"
-import App from "./App"
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import App from "./App";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
-  cache: new InMemoryCache()
-})
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   root.render(
     <React.StrictMode>
-      <ApolloProvider client={client} >
+      <ApolloProvider client={client}>
         <App />
       </ApolloProvider>
     </React.StrictMode>
-  )
-})
+  );
+});
