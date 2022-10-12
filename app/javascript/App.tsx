0b5aa56ba880dbdf739e66bf9966b998949e78c8
kmkkiii <rskkmkmk@gmail.com>
2022-10-12 11:02:33 +0900

    タスク一覧表示

--- app/javascript/App.tsx
+++ app/javascript/App.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { useFetchTasksQuery } from "./generated/graphql";
 
 const App: React.FC = () => {
+  const { loading, data } = useFetchTasksQuery();
+
+  console.log(data);
+  // if (error) return `エラー：${error.message}`;
+
   return (
     <>
-      <h1>Hello, World!</h1>
+      <h1>タスク一覧</h1>
+      {loading ? (
+        <p>Loading ...</p>
+      ) : (
+        <ul>
+          {data?.tasks.map((task) => (
+            <li key={task.id}>{task.title}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
