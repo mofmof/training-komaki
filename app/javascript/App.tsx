e60374ac9b1e82501e9165dc1ad711f13fd94cfc
kmkkiii <rskkmkmk@gmail.com>
2022-10-12 14:38:46 +0900

    デバッグ用コード削除

--- app/javascript/App.tsx
+++ app/javascript/App.tsx
@@ -4,9 +4,6 @@ import { useFetchTasksQuery } from "./generated/graphql";
 const App: React.FC = () => {
   const { loading, data } = useFetchTasksQuery();
 
-  console.log(data);
-  // if (error) return `エラー：${error.message}`;
-
   return (
     <>
       <h1>タスク一覧</h1>
