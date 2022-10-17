2ad8b205fddb24e948d08658d185581740f12859
kmkkiii <rskkmkmk@gmail.com>
2022-10-17 14:36:03 +0900

    タスク詳細取得関数追加

---
+++ app/javascript/graphql/queries/task.ts
@@ -0,0 +1,12 @@
+import { gql } from "@apollo/client";
+
+export const FETCH_TASK_BY_ID = gql`
+  query FetchTaskById($id: ID!) {
+    task(id: $id) {
+      id
+      title
+      detail
+      limitOn
+    }
+  }
+`;
