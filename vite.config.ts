3b56927a6a3219338fe6b8e628a7ce19e3b28a97
kmkkiii <rskkmkmk@gmail.com>
2022-10-18 12:54:12 +0900

    WindiCSSプラグイン追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import RubyPlugin from "vite-plugin-ruby";
 import FullReload from "vite-plugin-full-reload";
+import WindiCSS from "vite-plugin-windicss";
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
     FullReload(["config/routes.rb", "app/views/**/*"], { delay: 100 }),
+    WindiCSS({
+      root: "./",
+      scan: {
+        fileExtensions: ["erb", "haml", "html", "js", "ts", "jsx", "tsx"],
+        dirs: ["app/views", "app/javascript"],
+      },
+    }),
   ],
 });
