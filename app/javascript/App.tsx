a0918274b0a6d83f4f4893aa4f7ad9fefb8b3d18
kmkkiii <rskkmkmk@gmail.com>
2022-10-18 16:18:13 +0900

    Route追加

--- app/javascript/App.tsx
+++ app/javascript/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NewTask from "./components/NewTask";
 import ErrorPage from "./components/ErrorPage";
 import Task from "./components/Task";
 import TaskList from "./components/TaskList";
@@ -9,7 +10,8 @@ const App: React.FC = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TaskList />} />
-        <Route path="tasks/:id" element={<Task />} />
+        <Route path="/tasks/:id" element={<Task />} />
+        <Route path="/tasks/new" element={<NewTask />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
