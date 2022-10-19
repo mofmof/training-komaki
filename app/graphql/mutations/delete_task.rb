2a245685af8ed2784054d23e5b8b612118254fed
kmkkiii <rskkmkmk@gmail.com>
2022-10-19 11:27:41 +0900

    タスク削除Mutation追加

---
+++ app/graphql/mutations/delete_task.rb
@@ -0,0 +1,14 @@
+module Mutations
+  class DeleteTask < BaseMutation
+    field :id, ID, null: false
+
+    argument :id, ID, required: true
+
+    def resolve(id:)
+      ::Task.find(id).destroy
+      { id: }
+    rescue StandardError => e
+      GraphQL::ExecutionError.new(e.message)
+    end
+  end
+end
