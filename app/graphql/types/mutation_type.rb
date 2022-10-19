8b35aaeaf0c3a259243437b0ecc1e812c7f2b1a8
kmkkiii <rskkmkmk@gmail.com>
2022-10-19 11:24:58 +0900

    作業ブランチを間違えたのでrevert

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,5 @@
 module Types
   class MutationType < Types::BaseObject
-    field :delete_task, mutation: Mutations::DeleteTask
     field :create_task, mutation: Mutations::CreateTask
   end
 end
--- app/javascript/graphql/mutations/deleteTask.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const DELETE_TASK = gql`
-  mutation DeleteTask($id: ID!) {
-    deleteTask(input: { id: $id }) {
-      id
-    }
-  }
-`;
