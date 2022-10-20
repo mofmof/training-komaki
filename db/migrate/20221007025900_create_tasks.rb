e2c495cd018db0c25f315bfe1d5a9d56212a13a7
kmkkiii <rskkmkmk@gmail.com>
2022-10-20 17:11:28 +0900

    migrationファイル修正

--- db/migrate/20221007025900_create_tasks.rb
+++ db/migrate/20221007025900_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
       t.string :title, null: false
-      t.text :detail, null: false
+      t.text :detail
       t.date :limit_on, null: false
       t.timestamps
     end
