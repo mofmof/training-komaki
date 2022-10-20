4dc75cc7200e78e1d7c9efacd07f401bfdc7f308
kmkkiii <rskkmkmk@gmail.com>
2022-10-20 16:43:07 +0900

    Statusモデル追加

---
+++ db/migrate/20221020073331_add_status_id_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddStatusIdToTasks < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :tasks, :statuses, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_07_025900) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_073331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
@@ -20,6 +26,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_07_025900) do
     t.date "limit_on", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "statuses_id"
+    t.index ["statuses_id"], name: "index_tasks_on_statuses_id"
   end
 
+  add_foreign_key "tasks", "statuses", column: "statuses_id"
 end
