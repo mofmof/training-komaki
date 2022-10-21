7c3a19df9c8a6126a774db2a6da418e521b47e58
kmkkiii <rskkmkmk@gmail.com>
2022-10-21 11:22:40 +0900

    class名修正

--- db/migrate/20221020073331_add_status_id_to_tasks.rb
+++ db/migrate/20221020073331_add_status_id_to_tasks.rb
@@ -1,4 +1,4 @@
-class AddStatusIdToTasks < ActiveRecord::Migration[7.0]
+class AddStatusesIdToTasks < ActiveRecord::Migration[7.0]
   def change
     add_reference :tasks, :statuses, foreign_key: true
   end
