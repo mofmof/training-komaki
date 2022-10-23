38ff50eab986f4a51a249bba467871094c981c40
kmkkiii <rskkmkmk@gmail.com>
2022-10-24 08:54:51 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@ Status.create(name: "完了")
 
 # タスク追加
 10.times do |n|
-  Task.create(title: "タスク#{n + 1}", detail: "詳細#{n + 1}", limit_on: Time.zone.today + 3)
+  Task.create!(title: "タスク#{n + 1}", detail: "詳細#{n + 1}", limit_on: Time.zone.today + 3, status_id: 1)
 end
