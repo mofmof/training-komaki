d76504ee45a579a2e543fa25d111e9a849fbe5bc
kmkkiii <rskkmkmk@gmail.com>
2022-10-31 18:19:55 +0900

    サービス運営者用画面追加

--- app/javascript/components/RequireAuth.tsx
+++ app/javascript/components/RequireAuth.tsx
@@ -1,14 +1,28 @@
 import React, { useContext } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../App";
 import Header from "./Header";
 
-const RequireAuth: React.FC = () => {
-  const contextValue = useContext(AuthContext);
+export const Role = {
+  ADMIN: "admin",
+  GENERAL: "general",
+} as const;
 
-  if (!contextValue.isSignedIn) {
+const RequireAuth: React.FC = () => {
+  const { isSignedIn, currentUser } = useContext(AuthContext);
+
+  if (!isSignedIn) {
     return <Navigate to="/signin" />;
   }
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  if (currentUser?.role === Role.ADMIN) {
+    navigate("/users");
+  } else if (location.pathname === "/users") {
+    navigate("/");
+  }
+
   return (
     <>
       <Header />
