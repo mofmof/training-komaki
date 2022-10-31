f1785b8d50ce308d0537d676f556e8bbeb4bad52
kmkkiii <rskkmkmk@gmail.com>
2022-10-31 15:27:59 +0900

    userテーブルへrole追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,15 @@ statuses.each do |status|
   Status.find_or_create_by(status)
 end
 
+# 管理者ユーザー追加
+User.create!(name: "admin", email: "admin@example.com", password: "password", role: 1)
+
 # ユーザー追加
-User.create!(name: "test", email: "test@example.com", password: "password")
+User.create!(name: "test1", email: "test1@example.com", password: "password", role: 0)
+User.create!(name: "test2", email: "test2@example.com", password: "password", role: 0)
 
 # タスク追加
 10.times do |n|
-  Task.create!(title: "タスク#{n + 1}", detail: "詳細#{n + 1}", limit_on: Time.zone.today + 3, status_id: 1, user_id: 1)
+  Task.create!(title: "test1のタスク#{n + 1}", detail: "詳細#{n + 1}", limit_on: Time.zone.today + (n + 1), status_id: 1, user_id: 2)
+  Task.create!(title: "test2のタスク#{n + 1}", detail: "詳細#{n + 1}", limit_on: Time.zone.today + (n + 1), status_id: 1, user_id: 3)
 end
