4aadfb220fdb18f43d53397135234ce21de23936
kmkkiii <rskkmkmk@gmail.com>
2022-11-08 12:27:53 +0900

    mailgun設定追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,13 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: Rails.application.credentials.dig(:mailgun, :private_api_key),
+    domain: 'myapp-371n.onrender.com',
+    # api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
