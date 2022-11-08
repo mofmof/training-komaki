c510dacbb3dee031969a55c2a42b3435286c85dc
kmkkiii <rskkmkmk@gmail.com>
2022-11-08 11:00:17 +0900

    ボタンクリック時の処理修正

--- app/javascript/components/TaskList.tsx
+++ app/javascript/components/TaskList.tsx
@@ -111,7 +111,7 @@ const TaskList: React.FC = () => {
           className="text-sm mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           type="button"
           onClick={() => {
-            void exportTaskMutation;
+            void exportTaskMutation();
           }}
         >
           エクスポート
