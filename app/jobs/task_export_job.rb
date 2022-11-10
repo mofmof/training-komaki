069f397e965546366eadee717c82d87f53d37d2b
kmkkiii <rskkmkmk@gmail.com>
2022-11-10 09:34:55 +0900

    ActiveStorageを使ってURL取得するように修正

--- app/jobs/task_export_job.rb
+++ app/jobs/task_export_job.rb
@@ -19,31 +19,14 @@ class TaskExportJob < ApplicationJob
       end
     end
 
-    s3 = Aws::S3::Client.new(
-      access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-      secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      region: Rails.application.credentials.dig(:aws, :s3, :region),
+    csv = CsvUpload.create!(
+      file: ActiveStorage::Blob.create_and_upload!(
+        io: File.open("tmp/tmpfile.csv"),
+        filename: "tasks_#{Time.current.strftime('%Y_%m_%d')}.csv"
+      )
     )
 
-    # オブジェクトのキー
-    key = "export/tasks_#{Time.current.strftime('%Y_%m_%d')}.csv"
-
-    # S3へアップロード
-    s3.put_object(
-                  bucket: Rails.application.credentials.dig(:aws, :s3, :bucket),
-                  key: key,
-                  body: File.open("tmp/tmpfile.csv"),
-                  content_type: 'text/csv'
-                )
-
-    url = Aws::S3::Presigner.new(client: s3).presigned_url(
-      :get_object,
-      bucket: Rails.application.credentials.dig(:aws, :s3, :bucket),
-      key: key,
-      response_content_type: 'application/force-download'
-    )
-
-    CompleteMailer.complete_notification(user, "タスクのエクスポートが完了しました", url).deliver_now
+    CompleteMailer.complete_notification(user, "タスクのエクスポートが完了しました", csv.file.url).deliver_now
   rescue => e
     ErrorMailer.error_notification(user, e).deliver_now
   end
