320a2e992c60d1b144b5cbf9e323cba4d1e4afac
kmkkiii <rskkmkmk@gmail.com>
2022-11-11 11:36:26 +0900

    resolveにフィルタリング用引数を追加

--- app/graphql/queries/tasks.rb
+++ app/graphql/queries/tasks.rb
@@ -2,8 +2,13 @@ module Queries
   class Tasks < Queries::AuthRequiredQuery
     type ObjectTypes::TaskType.connection_type, null: false
 
-    def resolve
-      current_user.tasks.order(limit_on: :asc, created_at: :desc)
+    argument :from, String, required: false
+    argument :to, String, required: false
+
+    def resolve(**args)
+      tasks = current_user.tasks.order(limit_on: :asc, created_at: :desc)
+      tasks = tasks.where(limit_on: args[:from]..args[:to]) if args[:from].present? && args[:to].present?
+      tasks
     end
   end
 end
