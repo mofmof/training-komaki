561037d8b6cc31b770ac153f5d9b8086b5f16554
kmkkiii <rskkmkmk@gmail.com>
2022-11-25 11:04:21 +0900

    チームのタスク一覧取得Query追加

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,5 +10,6 @@ module Types
     field :users, resolver: Queries::Users
     field :teams, resolver: Queries::Teams
     field :team, resolver: Queries::Team
+    field :team_tasks, resolver: Queries::TeamTasks
   end
 end
